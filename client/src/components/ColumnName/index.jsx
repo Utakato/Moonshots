5dbe9fd37d759635db017897afbf8dc8d23abd9f
Utakato <copyresearch98@gmail.com>
2021-11-26 11:13:50 +0200

    Add ColumnName Component

---
+++ client/src/components/ColumnName/index.jsx
@@ -0,0 +1,10 @@
+import "./style.css"
+const ColumnName = ({name}) => {
+    return (
+        <div className="column-name">
+            {name}
+        </div>
+    )
+}
+
+export default ColumnName
