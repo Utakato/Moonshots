eaca37ff6ee1eab4a1bcedfbc3b317a94e595d9a
Utakato <copyresearch98@gmail.com>
2021-11-26 15:36:10 +0200

    Switch to axios

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,10 +1,7 @@
 const url = "http://localhost:5000/api/project"
 const axios = require("axios")
 
-export const fetchProjects = async () => {
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log({data})
-    return data
+export const fetchProjects = () => {
+    return axios.get(url)
 }
 
