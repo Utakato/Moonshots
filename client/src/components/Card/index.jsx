edbb615825354800e94b815714267727e95e83c6
Utakato <copyresearch98@gmail.com>
2021-11-26 16:06:05 +0200

    Format numbers

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -1,15 +1,17 @@
 import "./style.css"
 
 const Card = ({rank, symbol, name, price, mCap, watchlist, followers, link, twitter_link}) => {
+    const newMcap = mCap.toLocaleString().toString().slice(0,-8)
+    
     return (
         <div className="card">
             <div className="stats rank"> # {rank} </div>
             <div className="stats symbol"> {symbol}</div>
             <div className="stats name">{name}</div>
-            <div className="stats price">{price} $</div>
-            <div className="stats market-cap">{mCap} $</div>
-            <div className="stats watchlist">{watchlist}</div>
-            <div className="stats followers">{followers}</div>
+            <div className="stats price">{price.toLocaleString()} $</div>
+            <div className="stats market-cap">{newMcap}M $</div>
+            <div className="stats watchlist">{watchlist.toLocaleString()}</div>
+            <div className="stats followers">{followers.toLocaleString()}</div>
         </div>
     )
 }
