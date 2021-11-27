a22f8987e60838686f2200ba6d23ca16e194523f
Utakato <copyresearch98@gmail.com>
2021-11-27 12:16:20 +0200

    Add filter logic to top buttons

--- client/src/components/ColumnName/index.jsx
+++ client/src/components/ColumnName/index.jsx
@@ -1,8 +1,32 @@
 import "./style.css"
+import * as actions from "../../actions/queryActions"
+import {useSelector, useDispatch} from "react-redux"
+import { useEffect } from "react" 
+
 const ColumnName = ({name, value}) => {
-    return (
-        <div className="column-name">
-            {name}
+    const queryState = useSelector(state => state.query)
+	const dispatch = useDispatch()
+    
+    const handleToggleClick = () => {
+        let query = `${value}=-1`
+        dispatch(actions.updateQuery(query))
+    }
+    let sort = false
+    let sortDir = "fas fa-arrow-down"
+    
+    if (queryState.split("=")[0] == value) {
+        sort = true
+        if (queryState.split("=")[1] == "1" ) {
+            sortDir = "fas fa-arrow-up"
+        } else {
+            sortDir = "fas fa-arrow-down"
+        }
+    }
+
+    return (    
+        <div className="column-name" onClick={handleToggleClick}>
+            {name} {sort && <i class={sortDir}></i>}
+            
         </div>
     )
 }
