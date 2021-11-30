3ee9726d2214ff082fe23ab0d9b77c7b4a61fc80
Utakato <copyresearch98@gmail.com>
2021-11-30 14:22:28 +0200

    Add Header and Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import CardContainer from "./components/CardContainer";
 import Paginate from "./components/Paginate"
+import Header from "./components/Header"
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
+      <Header/>
     <CardContainer></CardContainer>
     <Paginate itemsPerPage={20}/>
+    <Footer/>
     </div>
   );
 }
