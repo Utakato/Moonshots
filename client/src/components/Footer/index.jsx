2a8b93d407b45ac59dfc228135c31ab4754b1edb
Utakato <copyresearch98@gmail.com>
2021-11-30 14:22:07 +0200

    Create Footer

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h2>Project created by <a href="https://github.com/Utakato" target="_blank">Utakato</a></h2>
+        </div>
+    )
+}
+
+export default Footer
